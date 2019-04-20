e1f27e989aeca9eb5f76d7ba795b38b097a8019d
Ronald Jerez <RonaldJerez@users.noreply.github.com>
2019-04-19 23:55:41 -0400

    test(app): start tracking tests

---
+++ __tests__/server.test.js
@@ -0,0 +1,95 @@
+const request = require('supertest');
+const Server = require('../src');
+
+let server
+beforeAll(async () => {
+  server = Server('./', 0, true)
+});
+
+afterAll(() => {
+  server.close();
+});
+
+test('non existing endpoint', () => request(server)
+  .get('/api/non/existant')
+  .expect(404)
+)
+
+test('endpoint with deeper path', () => request(server)
+  .get('/api/deep/endpoint')
+  .expect(201)
+)
+
+test('reads documents with a single mock', () => request(server)
+  .get('/api/single')
+  .expect(200, { color: 'blue' })
+)
+
+test('reads documents with multiple mocks', () => request(server)
+  .get('/api/multi')
+  .expect(400, { mock: 'fallback' })
+  .expect('x-custom', 'custom header value')
+)
+
+test('pick the first mock that matches', () => request(server)
+  .get('/api/multi?which=first')
+  .expect({mock: 'first'})
+)
+
+test('mocks with delay setting should be delayed', async () => {
+  const start = Date.now()
+  await request(server).get('/api/delay')
+  const end = Date.now()
+
+  expect(end - start).toBeGreaterThanOrEqual(200);
+})
+
+describe('Response Content-Type', () => {
+  test('regular json, application/json', () => request(server)
+    .get('/api/content?type=json')
+    .expect('Content-Type', /application\/json/)
+  )
+
+  test('yml to json -> application/json', () => request(server)
+    .get('/api/content?type=json2')
+    .expect('Content-Type', /application\/json/)
+  )
+
+  test('text/html', () => request(server)
+    .get('/api/content?type=html')
+    .expect('Content-Type', /text\/html/)
+  )
+
+  test('text/html', () => request(server)
+    .get('/api/content?type=text')
+    .expect('Content-Type', /text\/plain/)
+  )
+})
+
+describe('Matcher modifiers', () => {
+  test('.has: string value', () => request(server)
+    .get('/api/modifier')
+    .set('x-has-single', 'anything')
+    .expect(201)
+  )
+
+  test('.has: array value', () => request(server)
+    .get('/api/modifier')
+    .set('x-has-one', 'anything')
+    .set('x-has-two', 'anything')
+    .expect(202)
+  )
+
+  test('.equal', () => request(server)
+    .post('/api/modifier')
+    .send({one: 1})
+    .expect(204)
+  )
+
+  test('.equal, extra content', () => request(server)
+    .post('/api/modifier')
+    .send({one: 1, two: 2})
+    .expect(404)
+  )
+
+})
